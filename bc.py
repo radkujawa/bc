0da56b41b6f3aafe9e2b8c37c64791e7caa7f361
radek kujawa <rkujawa@starfishstorage.com>
2020-03-28 00:25:02 +0100

    add putting out the candles on fffff

--- bc.py
+++ bc.py
@@ -1,6 +1,9 @@
+import copy
 import math
 import random
-from copy import copy
+import sys
+import threading
+from queue import Queue
 
 from asciimatics.screen import Screen
 
@@ -16,8 +19,6 @@ def get_board_str(board):
     ret = []
     for i in board:
         s = ""
-        if not {c for k, c in board[i].items() if c != " "}:
-            continue
         for j in board[i]:
             s += board[i][j]
         ret.append(s)
@@ -96,7 +97,7 @@ def set_middle(board):
     set_on_board(middle, "%", board)
 
 
-def set_candle(board, x, y):
+def set_candle(board, x, y, fire, without_candles):
     for i in range(3):
         board[x - 2 * i][y] = "`"
         board[x - 2 * i - 1][y] = " "
@@ -115,11 +116,19 @@ def set_candle(board, x, y):
     board[x - 6][y - 1] = "'"
     board[x - 6][y + 1] = "'"
     board[x - 6][y] = "|"
-    board[x - 7][y - 1] = "("
-    board[x - 7][y + 1] = ")"
-    board[x - 7][y] = "o"
-    board[x - 8][y] = "("
-    board[x - 9][y] = ")"
+
+    if fire:
+        board[x - 7][y - 1] = "("
+        board[x - 7][y + 1] = ")"
+        board[x - 7][y] = "o"
+        board[x - 8][y] = "("
+        board[x - 9][y] = ")"
+    else:
+        board[x - 7][y - 1] = without_candles[x - 7][y - 1]
+        board[x - 7][y + 1] = without_candles[x - 7][y + 1]
+        board[x - 7][y] = without_candles[x - 7][y]
+        board[x - 8][y] = without_candles[x - 8][y]
+        board[x - 9][y] = without_candles[x - 9][y]
 
 
 class CakeTooSmall(Exception):
@@ -148,7 +157,7 @@ def set_candles(years, board):
             candidates.remove((x, y))
 
             fits = True
-            for candle_x, candle_y in candles:
+            for candle_x, candle_y, _ in candles:
                 if math.fabs(candle_y - y) < 4 and math.fabs(candle_x - x) < 12:
                     fits = False
                     break
@@ -156,11 +165,12 @@ def set_candles(years, board):
             if not fits:
                 continue
 
-            candles.add((x, y))
+            candles.add((x, y, True))
 
     if len(candles) == years:
-        for x, y in candles:
-            set_candle(board, x, y)
+        for x, y, fire in candles:
+            assert fire
+            set_candle(board, x, y, fire, None)
         return candles
     else:
         raise CakeTooSmall(f"not enough place to set {years} candles!")
@@ -168,13 +178,24 @@ def set_candles(years, board):
 
 def move_candles(candles, board):
     n = random.randint(0, int(len(candles) / 4))
-    to_move = copy(candles)
+    to_move = copy.deepcopy(candles)
 
     while n > 0:
-        x, y = random.choice(list(to_move))
-        to_move.remove((x, y))
-        board[x - 8][y], board[x - 9][y] = board[x - 9][y], board[x - 8][y]
+        x, y, fire = random.choice(list(to_move))
+        to_move.remove((x, y, fire))
+        if fire:
+            board[x - 8][y], board[x - 9][y] = board[x - 9][y], board[x - 8][y]
         n -= 1
+
+
+def put_out_a_candle(candles, board, without_candles):
+    firing = [(x, y, fire) for (x, y, fire) in candles if fire]
+    if firing:
+        x, y, fire = random.choice(firing)
+        assert fire
+        candles.remove((x, y, fire))
+        candles.add((x, y, False))
+        set_candle(board, x, y, False, without_candles)
 
 
 def create_board():
@@ -184,19 +205,35 @@ def create_board():
     }
 
 
-def fill_up_board(board, candles_number):
+def fill_up_board(board):
     set_plat(board)
     set_cake(board)
     set_coating(board)
     set_middle(board)
-    return set_candles(candles_number, board), board
+
+
+def add_input(input_queue):
+    while True:
+        input_queue.put(sys.stdin.read(1))
 
 
 def main(years):
-    candles, board = fill_up_board(create_board(), years)
+    board = create_board()
+    fill_up_board(board)
+    without_candles = copy.deepcopy(board)
+    candles = set_candles(years, board)
+
+    input_queue = Queue()
+    input_thread = threading.Thread(target=add_input, args=(input_queue,))
+    input_thread.daemon = True
+    input_thread.start()
 
     def print_board_ascii(screen):
         while True:
+            if not input_queue.empty():
+                char = input_queue.get()
+                if char == "f":
+                    put_out_a_candle(candles, board, without_candles)
             move_candles(candles, board)
             board_str = get_board_str(board)
             for i in range(len(board_str)):
